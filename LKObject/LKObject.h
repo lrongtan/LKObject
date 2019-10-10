41717b01c0c86f989a0b3472ba20dc5f11c5198a
LiRongTan <844928336@qq.com>
2019-10-10 14:04:05 +0800

    kj

---
+++ LKObject/LKObject.h
@@ -0,0 +1,23 @@
+//
+//  LKObject.h
+//  LKObject
+//
+//  Created by 李荣潭 on 2019/10/10.
+//  Copyright © 2019 Lezhizhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TxObject.h"
+
+#import "TestNumber.h"
+
+//! Project version number for LKObject.
+FOUNDATION_EXPORT double LKObjectVersionNumber;
+
+//! Project version string for LKObject.
+FOUNDATION_EXPORT const unsigned char LKObjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LKObject/PublicHeader.h>
+
+
